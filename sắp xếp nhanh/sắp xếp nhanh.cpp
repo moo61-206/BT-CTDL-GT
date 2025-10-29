f10b19df5e2ab058b0f72f97905d8b929fb2e9bb
Nguyen Nhat Hoang <nhathoang4anq@gmail.com>
2025-10-29 23:32:25 +0700

    BTVN 24/10

---
+++ sắp xếp nhanh/sắp xếp nhanh.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+void quickSort(int A[], int l, int r, int n) {
+    int p = A[l]; 
+    int i = l + 1;
+    int j = r;
+
+    while (i <= j) {
+        while (i <= r && A[i] < p) i++;
+        while (j >= l && A[j] > p) j--;
+
+        if (i <= j) {
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            for (int k = 0; k < n; k++) cout << A[k] << " ";
+            cout << endl;
+
+            i++;
+            j--;
+        }
+    }
+    int temp = A[l];
+    A[l] = A[j];
+    A[j] = temp;
+    for (int k = 0; k < n; k++) cout << A[k] << " ";   
+    cout <<  endl;
+
+    if (l < j - 1) quickSort(A, l, j - 1, n);
+    if (j + 1 < r) quickSort(A, j + 1, r, n);
+}
+
+int main() {
+    int A[] = { 3,10,1,23,103,34,17,53,64,71 };
+    int n = sizeof(A) / sizeof(A[0]);
+    quickSort(A, 0, n-1, n);
+
+    return 0;
+}
+
+
+
---
+++ sắp xếp trộn/sắp xếp trộn.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+using namespace std;
+const int MAX = 100; 
+int temp[MAX];
+void mergeParts(int* A, int l, int mid, int r, int n) {
+    int i = l, j = mid + 1, k = 0;
+    while (i <= mid && j <= r) {
+        int p;
+        if (A[i] < A[j]) p = A[i++];
+        else p = A[j++];
+        temp[k++] = p;
+    }
+    while (i <= mid) temp[k++] = A[i++];
+    while (j <= r) temp[k++] = A[j++];
+    for (int i = 0; i < k; i++) {
+        A[l + i] = temp[i];
+    }
+    for (int t = 0; t < n; t++) cout << A[t] << " ";
+    cout << endl;
+}
+void mergeSort(int* arr, int l, int r, int n) {
+    if (l >= r) return;
+    int mid = (l + r) / 2;
+    mergeSort(arr, l, mid, n);
+    mergeSort(arr, mid + 1, r, n);
+    mergeParts(arr, l, mid, r, n);
+}
+int main()
+{
+    int A[] = { 3,10,1,23,103,34,17,53,64,71 };
+    int n = sizeof(A) / sizeof(A[0]);
+    mergeSort(A, 0, n - 1, n );
+    return 0;
+
+
+}
